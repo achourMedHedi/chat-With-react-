7b0f3701562d6a38ce54867a9f2b25868a7993cb
Achour Mohamed Hedi <40775288+achourMedHedi@users.noreply.github.com>
2018-12-23 17:00:18 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    user1 : "" , 
+    user2 : "" ,
+    message : [ ]
+  }
+  render() {
+    return (
+      <div className="App">
+       <div>
+        {
+          this.state.message.map((el , index) => {
+            return (
+              <div >
+                <div style={el.id === 1 ? {float : "right" , color : 'red' } : {flaot : 'left' , color : 'blue'}} >
+                  {el.id == 1 ? "user 1 " : "user 2"} {el.message} 
+                </div>
+                <br/>
+                <br/>
+              </div>
+            )
+          })
+        }
+       </div>
+
+        <input type="text" onChange={(e) => this.setState({user1 : e.target.value})} placeholder="user 1 "/>
+        <button type="submit" onClick={() => this.setState({ message : this.state.message.concat({id : 1 , message : this.state.user1}) })}>send user 1 </button>
+        <input type="text" onChange={(e) => this.setState({user2 : e.target.value})} placeholder="user 2 "/>
+        <button type="submit" onClick={() => this.setState({ message : this.state.message.concat({id : 2 , message : this.state.user2}) })}>send user 1 </button>
+
+       
+      </div>
+    );
+  }
+}
+
+export default App;
